b30f8c05c581578663c5eda3d8a251731e19cb7c
Jair Andrade <jair.albert.andrade@gmail.com>
2020-04-23 14:15:44 +0100

    Remove dependency on external files for create_stan_function

--- tests/testthat/test-create_stan_function.R
+++ tests/testthat/test-create_stan_function.R
@@ -1,3 +1,84 @@
+context("Create Stan function")
+
+lv <- # Lotka-Volterra
+  '<root>
+      <doc1 xmlns = "http://docs.oasis-open.org/xmile/ns/XMILE/v1.0">
+        <header>
+		      <vendor>isee systems, inc.</vendor>
+		    </header>
+	      <sim_specs>
+	        <start>0</start>
+		      <stop>4</stop>
+		      <dt reciprocal="true">4</dt>
+	      </sim_specs>
+	   	  <variables>
+	   	    <stock name="x">
+				    <eqn>10</eqn>
+				    <inflow>Bx</inflow>
+				    <outflow>Dx</outflow>
+			    </stock>
+			    <flow name="Bx">
+				    <eqn>a * x</eqn>
+			    </flow>
+			    <flow name="Dx">
+				    <eqn>b * x * y</eqn>
+			    </flow>
+			    <stock name="y">
+				    <eqn>2</eqn>
+				    <inflow>By</inflow>
+				    <outflow>Dy</outflow>
+			    </stock>
+			    <flow name="By">
+				    <eqn>c * x * y</eqn>
+			    </flow>
+			    <flow name="Dy">
+				    <eqn>d * y</eqn>
+			    </flow>
+			    <aux name="c">
+				    <eqn>0.04</eqn>
+			    </aux>
+			    <aux name="d">
+				    <eqn>0.5</eqn>
+			    </aux>
+			    <aux name="b">
+				    <eqn>0.2</eqn>
+			    </aux>
+			    <aux name="a">
+				    <eqn>1</eqn>
+			    </aux>
+        </variables>
+      </doc1>
+    </root>'
+
+test_model <-
+  '<root>
+      <doc1 xmlns = "http://docs.oasis-open.org/xmile/ns/XMILE/v1.0">
+        <header>
+		      <vendor>isee systems, inc.</vendor>
+		    </header>
+	      <sim_specs>
+	        <start>0</start>
+		      <stop>4</stop>
+		      <dt reciprocal="true">4</dt>
+	      </sim_specs>
+	   	  <variables>
+	   	    <stock name="population">
+				    <eqn>100</eqn>
+				    <inflow>births</inflow>
+			    </stock>
+			    <flow name="births">
+				    <eqn>population * birthRate</eqn>
+			    </flow>
+			    <aux name="birthRate">
+				    <eqn>birthRate2</eqn>
+			    </aux>
+			    <aux name="birthRate2">
+				    <eqn>0.1</eqn>
+			    </aux>
+        </variables>
+      </doc1>
+    </root>'
+
 
 test_that("create_stan_function() returns the expected string", {
   expected_stan_function <- paste(
@@ -23,36 +104,31 @@ test_that("create_stan_function() returns the expected string", {
     "}",
     sep = "\n")
 
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "lotka_volterra")
+  stan_function <- create_stan_function(lv, "lotka_volterra")
   expect_equal(stan_function, expected_stan_function)
 })
 
 test_that("create_stan_function() returns a string", {
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "test_model")
+  stan_function <- create_stan_function(lv, "test_model")
   expect_is(stan_function, "character")
 })
 
 test_that("create_stan_function() assign the expected name to the function", {
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "lotka_volterra")
+  stan_function <- create_stan_function(lv, "lotka_volterra")
   second_line <- strsplit(stan_function, "\n")[[1]][[2]]
   expected_line <-   "  real[] lotka_volterra(real t,"
   expect_equal(second_line, expected_line)
 })
 
 test_that("create_stan_function() declares the exact number of stocks", {
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "lotka_volterra")
+  stan_function <- create_stan_function(lv, "lotka_volterra")
   seventh_line <- strsplit(stan_function, "\n")[[1]][[7]]
   expected_line <-   "  real dydt[2];"
   expect_equal(seventh_line, expected_line)
 })
 
 test_that("create_stan_function() declares the expected variables", {
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "lotka_volterra")
+  stan_function <- create_stan_function(lv, "lotka_volterra")
   eighth_line   <- strsplit(stan_function, "\n")[[1]][[8]]
   ninth_line    <- strsplit(stan_function, "\n")[[1]][[9]]
   tenth_line    <- strsplit(stan_function, "\n")[[1]][[10]]
@@ -68,8 +144,7 @@ test_that("create_stan_function() declares the expected variables", {
 })
 
 test_that("create_stan_function() generates the expected auxiliar equations", {
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "lotka_volterra")
+  stan_function       <- create_stan_function(lv, "lotka_volterra")
   actual_12th_line    <- strsplit(stan_function, "\n")[[1]][[12]]
   actual_13th_line    <- strsplit(stan_function, "\n")[[1]][[13]]
   actual_14th_line    <- strsplit(stan_function, "\n")[[1]][[14]]
@@ -86,8 +161,7 @@ test_that("create_stan_function() generates the expected auxiliar equations", {
 })
 
 test_that("create_stan_function() generates the expected differential equations", {
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "lotka_volterra")
+  stan_function <- create_stan_function(lv, "lotka_volterra")
   actual_16th_line    <- strsplit(stan_function, "\n")[[1]][[16]]
   actual_17th_line    <- strsplit(stan_function, "\n")[[1]][[17]]
   expected_16th_line  <-   "  dydt[1] = Bx-Dx;"
@@ -98,8 +172,7 @@ test_that("create_stan_function() generates the expected differential equations"
 
 
 test_that("create_stan_function() parameterise the function", {
-  file <- "./lotka_volterra.stmx"
-  stan_function <- create_stan_function(file, "lotka_volterra",
+  stan_function <- create_stan_function(lv, "lotka_volterra",
                                         pars = c("d", "a"))
   actual_12th_line   <- strsplit(stan_function, "\n")[[1]][[12]]
   actual_15th_line   <- strsplit(stan_function, "\n")[[1]][[15]]
@@ -110,8 +183,7 @@ test_that("create_stan_function() parameterise the function", {
 })
 
 test_that("create_stan_function() returns equations in computational  order", {
-  file           <- "./pop_extended_consts.stmx"
-  stan_function <- create_stan_function(file, "test_model")
+  stan_function <- create_stan_function(test_model, "test_model")
 
   expected_function <- paste(
     "functions {",
@@ -134,10 +206,9 @@ test_that("create_stan_function() returns equations in computational  order", {
 })
 
 test_that("create_stan_function() allows user to override constant values", {
-  file <- "./lotka_volterra.stmx"
   consts <- list(list(name = "a", value = 0.5), list(name = "c", value = 0.3))
   stan_function <- create_stan_function(
-    filepath = file, func_name = "lotka_volterra", override.consts = consts)
+    filepath = lv, func_name = "lotka_volterra", override.consts = consts)
 
   expected_stan_function <- paste(
     "functions {",
