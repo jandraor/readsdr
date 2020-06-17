5c459b09cee4b721ee8bea42cffebe1a2cadd7d2
Jair Andrade <jair.albert.andrade@gmail.com>
2020-06-17 19:56:12 +0100

    Add extract_timeseries_var()

--- R/stan_utils.R
+++ R/stan_utils.R
@@ -1,12 +1,44 @@
 
-# Extract variables over time
+#' Extract the values over time of a variable from a Stan fit
+#'
+#' @param var_name A string that indicates the variable's name for which the
+#' function will construct the timeseries.
+#' @param posterior_df A Stan fit object converted into a data frame
+#'
+#' @return A data frame
+#' @export
+#'
+#' @examples
+#' posterior_df <- data.frame(`var[1]` = rep(0, 2), `var[2]` = rep(1, 2),
+#'                             check.names = FALSE)
+#' extract_timeseries_var("var", posterior_df)
+extract_timeseries_var <- function(var_name, posterior_df) {
+  posterior_cols <- colnames(posterior_df)
+  pattern        <- stringr::str_glue("{var_name}\\[.+\\]")
+  pos_search     <- grep(pattern, posterior_cols)
+  search_cols    <- posterior_cols[pos_search]
+  search_df      <- posterior_df[, search_cols]
+
+  var_ts         <- purrr::imap_dfr(search_df, function(col, label) {
+    pattern      <- "\\[(\\d+)\\]"
+    match_output <- stringr::str_match(label, pattern)
+    time_var     <- as.numeric(match_output[[2]])
+
+    data.frame(stringsAsFactors = FALSE,
+               iter  = seq_along(col),
+               time  = time_var,
+               value = col)
+  })
+
+  var_ts$variable <- var_name
+  var_ts          <- var_ts[ , c(1:2,4, 3)]
+
+  var_ts
+}
 
 # Extract stock over time
 
 # Get stock inits
-
-# get transformed data block
-
 
 #' Stan's transformed data block for ODE models
 #'
--- tests/testthat/test-stan_utils.R
+++ tests/testthat/test-stan_utils.R
@@ -6,3 +6,17 @@ test_that("stan_transformed_data() returns the expected string", {
     "}", sep = "\n")
   expect_equal(stan_transformed_data(), expected_string)
 })
+
+test_that("extract_timeseries_var() returns the expected data frame", {
+  test_df <- data.frame(`var[1]` = rep(0, 2),
+                        `var[2]` = rep(1, 2),
+                        check.names = FALSE)
+
+  expected_df <- data.frame(iter     = rep(1:2, 2),
+                            time     = rep(1:2, each = 2),
+                            variable = "var",
+                            value    = c(0, 0, 1, 1))
+
+  expect_equal(extract_timeseries_var("var", test_df), expected_df)
+
+})
