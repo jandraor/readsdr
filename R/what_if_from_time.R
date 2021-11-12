4f3ca4aa71f6add1d6f0dac0081ff61bf707f66f
Jair Andrade <jair.albert.andrade@gmail.com>
2021-11-12 12:35:19 +0000

    Add par up_to_time to what_if_from time

--- R/what_if_from_time.R
+++ R/what_if_from_time.R
@@ -1,6 +1,7 @@
 #' What if from time t we change the value of some parameters
 #'
 #' @param time Time at which the parameter values change
+#' @param up_to_time Time from which the original values are restored.
 #' @param par_list A list that indicates which parameters change from time t.
 #'   For instance, if you wanted to change the value of parameter \code{c} to 4,
 #'   you would provide the \code{list(c = 4)}
@@ -15,9 +16,11 @@
 #'   mdl            <- read_xmile(filepath)
 #'   ds_components  <- mdl$deSolve_components
 #'   output         <- sd_what_if_from_time(3, list(c = 4), ds_components)
-sd_what_if_from_time <- function(time, par_list, ds_inputs,
-                               start_time = NULL, stop_time = NULL,
-                               timestep = NULL, integ_method = "euler") {
+sd_what_if_from_time <- function(time, up_to_time = Inf, par_list, ds_inputs,
+                                 start_time = NULL, stop_time = NULL,
+                                 timestep = NULL, integ_method = "euler") {
+
+  unchanged_consts <- ds_inputs$consts
 
   if(!is.null(start_time)) {
     ds_inputs$sim_params$start <- start_time
@@ -25,7 +28,10 @@ sd_what_if_from_time <- function(time, par_list, ds_inputs,
 
   if(!is.null(stop_time)) {
     ds_inputs$sim_params$stop <- stop_time
+  } else {
+    stop_time <- ds_inputs$sim_params
   }
+
   if(!is.null(timestep)) {
     ds_inputs$sim_params$dt <- timestep
   }
@@ -38,14 +44,33 @@ sd_what_if_from_time <- function(time, par_list, ds_inputs,
   last_row             <- utils::tail(first_run, 1)
   stk_names            <- names(ds_inputs$stocks)
   new_stocks           <- last_row[, stk_names] %>% unlist()
-  ds_inputs$stocks <- new_stocks
+  ds_inputs$stocks     <- new_stocks
 
   par_names                       <- names(par_list)
   ds_inputs$consts[par_names]     <- unlist(par_list)
 
-  second_run <- sd_simulate(ds_inputs, start_time = mid_time,
+
+  sr_stop_time <- min(up_to_time, stop_time) # second run stop time
+
+  second_run <- sd_simulate(ds_inputs,
+                            start_time   = mid_time,
+                            stop_time    = sr_stop_time,
                             integ_method = integ_method)
 
-  rbind(utils::head(first_run, -1), second_run)
+  current_output <- rbind(utils::head(first_run, -1), second_run)
 
+  if(is.infinite(up_to_time)) return(current_output)
+
+  last_row             <- utils::tail(current_output, 1)
+  stk_names            <- names(ds_inputs$stocks)
+  new_stocks           <- last_row[, stk_names] %>% unlist()
+  ds_inputs$stocks     <- new_stocks
+  ds_inputs$consts     <- unchanged_consts
+
+  third_run <- sd_simulate(ds_inputs,
+                           start_time   = sr_stop_time,
+                           stop_time    = stop_time,
+                           integ_method = integ_method)
+
+  rbind(utils::head(current_output, -1), third_run)
 }
--- tests/testthat/test-what_if_from_time.R
+++ tests/testthat/test-what_if_from_time.R
@@ -3,11 +3,31 @@ test_that("sd_what_if_from_time() returns the expected output", {
   filepath       <- system.file("models/", "SIR.stmx", package = "readsdr")
   mdl            <- read_xmile(filepath)
   ds_components  <- mdl$deSolve_components
-  output         <- sd_what_if_from_time(3, list(c = 4), ds_components,
-                                       start_time = 0, stop_time = 5,
-                                       integ_method = "rk4", timestep = 1 / 16)
+  output         <- sd_what_if_from_time(3, par_list = list(c = 4),
+                                         ds_inputs = ds_components,
+                                         start_time = 0, stop_time = 5,
+                                         integ_method = "rk4",
+                                         timestep = 1 / 16)
   expect_is(output, "data.frame")
 
   actual_val <- tail(output[, "Infected"], 1)
   expect_equal(actual_val , 253.706683332, tolerance = 1e-9)
+})
+
+test_that("sd_what_from_time() works with the up_to_time parameter", {
+
+  filepath       <- system.file("models/", "SIR.stmx", package = "readsdr")
+  mdl            <- read_xmile(filepath)
+  ds_components  <- mdl$deSolve_components
+  output         <- sd_what_if_from_time(time = 3, up_to_time = 5,
+                                         par_list = list(c = 4),
+                                         ds_inputs = ds_components,
+                                         start_time = 0, stop_time = 10,
+                                         integ_method = "rk4",
+                                         timestep = 1 / 16)
+
+  expect_is(output, "data.frame")
+
+  actual_val <- tail(output[, "Infected"], 1)
+  expect_equal(actual_val, 63.4511067818, tolerance = 1e-9)
 })
