6aa9cd62d1bc276149d09838aba5c9f157663365
Jair Andrade <jair.albert.andrade@gmail.com>
2022-07-26 18:51:05 +0100

    Add test for stock trans

--- tests/testthat/test-simulator_measurements.R
+++ tests/testthat/test-simulator_measurements.R
@@ -21,9 +21,6 @@ test_that("sd_measurements() returns the expected data frame", {
                             timestep     = 1/16,
                             integ_method = "rk4")
 
-  filepath       <- system.file("models/", "SEIR.stmx", package = "readsdr")
-  mdl            <- read_xmile(filepath)
-
   output          <- sd_simulate(mdl$deSolve_components, start_time = 0,
                                  stop_time = 10, timestep = 1/16,
                                  integ_method = "rk4")
@@ -66,9 +63,6 @@ test_that("sd_measurements() returns the expected data frame for the measurement
                             timestep     = 1/16,
                             integ_method = "rk4")
 
-  filepath       <- system.file("models/", "SEIR.stmx", package = "readsdr")
-  mdl            <- read_xmile(filepath)
-
   output          <- sd_simulate(mdl$deSolve_components,
                                  start_time = 0,
                                  stop_time = 10, timestep = 1/16,
@@ -88,4 +82,48 @@ test_that("sd_measurements() returns the expected data frame for the measurement
 
   expect_equal(actual, expected)
 
+})
+
+test_that("sd_measurements() returns the expected data frame for the measurements of the log of a stock", {
+
+  filepath       <- system.file("models/", "SEIR.stmx", package = "readsdr")
+  mdl            <- read_xmile(filepath)
+
+  set.seed(111)
+
+  mm1 <- list(stock_name = "C",
+              meas_type  = "as_is",
+              dist       = list(name        = "rlnorm",
+                                stock_par   = "meanlog",
+                                stock_trans = "log",
+                                extra_pars  = list(sdlog = 0.25)))
+
+  meas_model <- list(mm1)
+
+  actual <- sd_measurements(n_meas       = 2,
+                            ds_inputs    = mdl$deSolve_components,
+                            meas_model   = meas_model,
+                            start_time   = 0,
+                            stop_time    = 10,
+                            timestep     = 1/16,
+                            integ_method = "rk4")
+
+  output          <- sd_simulate(mdl$deSolve_components,
+                                 start_time = 0,
+                                 stop_time = 10, timestep = 1/16,
+                                 integ_method = "rk4")
+
+  output <- output[output$time %in% 0:10, ]
+
+  set.seed(111)
+
+  y1 <- rlnorm(n = nrow(output), meanlog =  log(output$C), sdlog = 0.25)
+  y2 <- rlnorm(n = nrow(output), meanlog =  log(output$C), sdlog = 0.25)
+
+  expected <- data.frame(iter        = rep(1:2, each = 11),
+                         time        = rep(0:10, 2),
+                         var_name    = "C",
+                         measurement = c(y1, y2))
+
+  expect_equal(actual, expected)
 })
