3d3269886bd1db3aeecdeea6ccb6035888885795
Jair Andrade <jair.albert.andrade@gmail.com>
2022-09-14 18:15:23 +0100

    Update SEIR age

--- tests/testthat/test-sd_bayes.R
+++ tests/testthat/test-sd_bayes.R
@@ -118,6 +118,22 @@ test_that("sd_Bayes() returns the expected file with data inits defined", {
   expect_equal(actual, expected)
 })
 
+test_that("sd_Bayes() returns the expected file for vectorised model", {
+
+  # filepath <- system.file("models/", "SEIR_age.stmx", package = "readsdr")
+  #
+  # ag <- c("A", "B", "C", "D") # age_groups
+  #
+  # measurements <- stringr::str_glue("y_{ag} ~ poisson(net_flow(C_{ag}))")
+  # meas_mdl     <- as.list(measurements)
+  #
+  # estimated_params <- list(sd_prior("par_rho", "beta", c(2, 2)))
+  #
+  # actual <- sd_Bayes(filepath = filepath,
+  #                    meas_mdl = meas_mdl,
+  #                    estimated_params = estimated_params)
+})
+
 test_that("extract_extra_params() returns the expected list", {
 
   meas_obj <- "y ~ neg_binomial_2(net_flow(C), phi)"
