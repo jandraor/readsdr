a8c393c66b79b739f7ab869ea6c7d4a2fb35c3e8
Jair Andrade <jair.albert.andrade@gmail.com>
2022-12-21 15:42:24 -0500

    Add lines in Stan GC for generating simulated measurements

--- tests/testthat/test-stan_gc.R
+++ tests/testthat/test-stan_gc.R
@@ -8,7 +8,9 @@ test_that("stan_gc() returns the expected string for a net flow measurement", {
   expected <- paste(
     "generated quantities {",
     "  real log_lik;",
+    "  array[n_obs] int sim_y;",
     "  log_lik = neg_binomial_2_lpmf(y | delta_x_1, phi);",
+    "  sim_y = neg_binomial_2_rng(delta_x_1, phi);",
     "}", sep = "\n")
 
   expect_equal(actual, expected)
@@ -19,8 +21,10 @@ test_that("stan_gc() returns the expected string for a net flow measurement", {
     "generated quantities {",
     "  real log_lik;",
     "  real log_lik_pred;",
+    "  array[n_obs] int sim_y;",
     "  log_lik = neg_binomial_2_lpmf(y | delta_x_1, phi);",
     "  log_lik_pred = neg_binomial_2_lpmf(y_ahead | y_pred, phi);",
+    "  sim_y = neg_binomial_2_rng(delta_x_1, phi);",
     "}", sep = "\n")
 
   expect_equal(actual, expected)
@@ -31,7 +35,9 @@ test_that("stan_gc() returns the expected string for a net flow measurement", {
   expected <- paste(
     "generated quantities {",
     "  real log_lik;",
+    "  array[n_obs] int sim_y;",
     "  log_lik = poisson_lpmf(y | delta_x_1);",
+    "  sim_y = poisson_rng(delta_x_1);",
     "}", sep = "\n")
 
   expect_equal(actual, expected)
@@ -42,7 +48,9 @@ test_that("stan_gc() returns the expected string for a net flow measurement", {
   expected <- paste(
     "generated quantities {",
     "  real log_lik;",
+    "  array[n_obs] real sim_y;",
     "  log_lik = normal_lpdf(y | delta_x_1, tau);",
+    "  sim_y = normal_rng(delta_x_1, tau);",
     "}", sep = "\n")
 
   expect_equal(actual, expected)
@@ -58,7 +66,9 @@ test_that("stan_gc() returns the expected string for a stock measurement", {
   expected <- paste(
     "generated quantities {",
     "  real log_lik;",
+    "  array[n_obs] int sim_y;",
     "  log_lik = poisson_lpmf(y | x[:, 5]);",
+    "  sim_y = poisson_rng(x[:, 5]);",
     "}", sep = "\n")
 
   expect_equal(actual, expected)
@@ -70,8 +80,10 @@ test_that("stan_gc() returns the expected string for a stock measurement", {
     "generated quantities {",
     "  real log_lik;",
     "  real log_lik_pred;",
+    "  array[n_obs] int sim_y;",
     "  log_lik = poisson_lpmf(y | x[:, 5]);",
     "  log_lik_pred = poisson_lpmf(y_ahead | y_pred);",
+    "  sim_y = poisson_rng(x[:, 5]);",
     "}", sep = "\n")
 
   expect_equal(actual, expected)
@@ -93,7 +105,15 @@ test_that("stan_gc() returns the expected string for a vectorised net flow measu
   expected <- paste(
     "generated quantities {",
     "  real log_lik;",
+    "  array[n_obs] int sim_y_A;",
+    "  array[n_obs] int sim_y_B;",
+    "  array[n_obs] int sim_y_C;",
+    "  array[n_obs] int sim_y_D;",
     "  log_lik = poisson_lpmf(y_A | delta_x_1)+poisson_lpmf(y_B | delta_x_2)+poisson_lpmf(y_C | delta_x_3)+poisson_lpmf(y_D | delta_x_4);",
+    "  sim_y_A = poisson_rng(delta_x_1);",
+    "  sim_y_B = poisson_rng(delta_x_2);",
+    "  sim_y_C = poisson_rng(delta_x_3);",
+    "  sim_y_D = poisson_rng(delta_x_4);",
     "}", sep = "\n")
 
   expect_equal(actual, expected)
@@ -105,6 +125,19 @@ test_that("get_log_lik_statement() returns the expected string", {
   actual   <- get_log_lik_statement(meas_obj, FALSE)
 
   expected <- "neg_binomial_2_lpmf(y | delta_x_1, phi);"
+
+  expect_equal(actual, expected)
+})
+
+test_that("generate_sim_data_lines() returns the expected list", {
+
+  meas_mdl  <- list("y ~ neg_binomial_2(net_flow(C), phi)")
+  lvl_names <- c("S", "E", "I", "R", "C")
+
+  actual <- generate_sim_data_lines(meas_mdl, lvl_names)
+
+  expected <- list(decl   = "  array[n_obs] int sim_y;",
+                   assign = "  sim_y = neg_binomial_2_rng(delta_x_1, phi);")
 
   expect_equal(actual, expected)
 })
